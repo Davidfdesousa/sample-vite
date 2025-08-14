1861c970ea0651014a078c2b82ec7a257da87b3f
davidfdesousa <davidfdesousa@hotmail.com>
2025-08-14 17:58:08 -0300

    feat: adicinonando novo logo do vite

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
